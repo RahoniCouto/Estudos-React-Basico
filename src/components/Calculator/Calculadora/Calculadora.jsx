3dfa60a5a69acdcb5120f5c0d874d855a570b66b
Rahoni Couto <djlokijoker@gmail.com>
2020-08-27 12:10:40 -0300

    Calculadora #02 - Back-End completo

--- src/components/Calculator/Calculadora/Calculadora.jsx
+++ src/components/Calculator/Calculadora/Calculadora.jsx
@@ -3,7 +3,18 @@ import './Calculadora.css';
 import Button from '../Button/Button';
 import Display from '../Display/Display';
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    oeration: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculadora extends Component {
+
+    state = { ...initialState }
+
     constructor(props) {
         super(props)
 
@@ -13,15 +24,68 @@ export default class Calculadora extends Component {
     }
 
     clearMemory() {
-        console.log('limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if( this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const finalOperation = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [ ...this.state.values]
+            
+            values[0] = () => {
+                let resultado = 0;
+                switch(currentOperation){
+                    case '+':
+                        resultado=values[0] + values[1];
+                    break;
+                    case '-':
+                        resultado=values[0] - values[1];
+                    break;
+                    case '*':
+                        resultado=values[0] * values[1];
+                    break;
+                    case '/':
+                        resultado=values[0] / values[1];
+                    break;
+                    default:
+                        resultado=0
+                }
+                return resultado
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: finalOperation ? null : operation,
+                current: finalOperation ? 0 : 1,
+                clearDisplay: !finalOperation,
+                values
+            })
+        }
     }
  
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [ ...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     }
 
     render() {
@@ -29,7 +93,7 @@ export default class Calculadora extends Component {
             <div className="Calculadora">
                 
                 <div className="Calculator">
-                    <Display value={0} />
+                    <Display value={ this.state.displayValue} />
                     <Button label="AC" click={this.clearMemory} triple operation />
                     <Button label="/" click={this.setOperation} operation  />
                     <Button label="7" click={this.addDigit} />
